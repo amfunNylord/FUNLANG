887462665e5ced8548326b81a8c241ab0b3778aa
amfunNyLORD <funnybulletgames@yandex.ru>
2024-03-24 14:54:08 +0300

    Правки по while и for

--- FUNLANG/ListstmntsParser.h
+++ FUNLANG/ListstmntsParser.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include <string>
+#include "WhileParser.h"
+#include "ForParser.h"
+#include "IFParser.h"
 
 bool LISTSTMNTSParse(std::string& code);
 
@@ -48,20 +51,20 @@ bool STParse(std::string& code)
 	{
 		return false;
 	}
-	//else if (code.substr(0, 5) == "WHILE") // use const
-	//{
-	//	if (!WHILEParse(code))
-	//	{
-	//		return false;
-	//	}
-	//}
-	//else if (code.substr(0, 3) == "FOR") // use const
-	//{
-	//	if (!FORParse(code))
-	//	{
-	//		return false;
-	//	}
-	//}
+	else if (code.substr(0, 5) == WHILE_START_TERMINAL) // use const
+	{
+		if (!WHILEParse(code))
+		{
+			return false;
+		}
+	}
+	else if (code.substr(0, 3) == "FOR") // use const
+	{
+		if (!FORParse(code))
+		{
+			return false;
+		}
+	}
 	//else if (code.substr(0, 2) == "IF") // use const
 	//{
 	//	if (!IFParse(code))
--- FUNLANG/WhileParser.h
+++ FUNLANG/WhileParser.h
@@ -1,50 +1,40 @@
 #pragma once
 #include <iostream>
+#include "EXPRParse.h"
+
 const std::string WHILE_START_TERMINAL = "WHILE";
-const std::string DO_TERMINAL = "DO";
 const std::string WHILE_END_TERMINAL = "ENDWHILE";
 
-bool PawrseWhile(std::string& str)
+bool STParse(std::string& code);
+
+bool WHILEParse(std::string& code)
 {
-	if (str.substr(0, WHILE_START_TERMINAL.length()) != WHILE_START_TERMINAL)
+	if (code.substr(0, 5) != WHILE_START_TERMINAL)
+	{
+		std::cout << "Syntax error: expected \'" + WHILE_START_TERMINAL + "\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	code.erase(0, 5);
+	if (!EXPRParse(code))
+	{
+		std::cout << "Syntax error: expected \'identifier\', line: pos:" << std::endl; // from lexer
+		return false;
+	}
+	if (code.substr(0, 2) != "DO")
 	{
 		return false;
 	}
-	str.erase(0, WHILE_START_TERMINAL.length());
-
-	size_t positionDo = str.find(DO_TERMINAL);
-	if (positionDo == std::string::npos)
+	code.erase(0, 2);
+	if (!STParse(code))
 	{
+		std::cout << "Syntax error: expected \'statement\', line: pos:" << std::endl; // from lexer
 		return false;
 	}
-	if (!Expr(str.substr(0, positionDo)))
+	if (code.substr(0, 8) != WHILE_END_TERMINAL)
 	{
+		std::cout << "Syntax error: expected \'" + WHILE_END_TERMINAL + "\', line: pos:" << std::endl; // from lexer
 		return false;
 	}
-	str.erase(0, positionDo);
-
-	if (str.substr(0, DO_TERMINAL.length()) != DO_TERMINAL)
-	{
-		return false;
-	}
-	str.erase(0, DO_TERMINAL.length());
-
-	size_t positionEndWhile = str.find(WHILE_END_TERMINAL);
-	if (positionEndWhile == std::string::npos)
-	{
-		return false;
-	}
-	if (!ParseSt(str.substr(0, positionEndWhile)))
-	{
-		return false;
-	}
-	str.erase(0, positionEndWhile);
-
-	if (str.substr(0, WHILE_END_TERMINAL.length()) != WHILE_END_TERMINAL)
-	{
-		return false;
-	}
-	str.erase(0, WHILE_END_TERMINAL.length());
-
+	code.erase(0, 8);
 	return true;
 }
